0f30f36c8d4ab2c59f09c58c211b494904a2fd46
Tushar12-byte <dubeytejash912@gmail.com>
2024-10-02 14:48:36 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,27 +1,13 @@
-package com.ComparableAndComparator;
-
-import java.util.ArrayList;
-import java.util.Collections;
+package com.genrics;
 
 public class Main {
-	public static void main(String[] args) {
-		//arrayList
-		ArrayList<Student> a1=new ArrayList<Student>();
-		
-		//add
-		a1.add(new Student(101,"Tejash",21));
-		a1.add(new Student(102,"Rohit",28));
-		a1.add(new Student(103,"Mohit",19));
-		a1.add(new Student(104,"Ram",25));
-		a1.add(new Student(105,"Raj",20));
-		
-		//sorting
-		Collections.sort(a1);
-		
-		//for
-		for(Student obj : a1) {
-			System.out.println(obj.rollno+" "+obj.name+" "+obj.age); 
-		}
-	}
+    public static void main(String[] args) {
+        // Instance of Integer type
+        Test<Integer> iobj = new Test<Integer>(15);
+        System.out.println(iobj.getObject());
 
+        // Instance of String type
+        Test<String> sobj = new Test<String>("Hello");
+        System.out.println(sobj.getObject());
+    }
 }
---
+++ Test.java
@@ -0,0 +1,17 @@
+package com.genrics;
+
+public class Test<T> {
+    //An object of type T is declared
+	T obj;
+	Test(T obj)
+	{
+		this.obj = obj;
+	}
+    //constructor
+	public T getObject()
+	{
+		return this.obj;
+	}
+}
+
+
